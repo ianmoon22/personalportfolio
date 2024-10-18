4248411dd93ec21fc9f4a358399f8ddd289e318c
ianmoon2022 <ianmoon2022@gmail.com>
2024-10-18 13:28:51 -0700

    trying stuff

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // alert("Welcome to my portfolio!");
 });
+
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 80 },
+        size: { value: 3 },
+        move: { speed: 2 },
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: "repulse" },
+        },
+    },
+});
